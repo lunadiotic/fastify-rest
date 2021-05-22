f2d21dd97b4d517b3247d527cdaed84f28984735
lionatic <ahmadhakim404@gmail.com>
2021-05-23 00:55:19 +0700

    create a REST API to Get All Data

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,5 +1,14 @@
 const Product = require('../models/products.model')
 
+exports.index = async (req, reply) => {
+    try {
+        const products = await Product.find()
+        return products
+    } catch (err) {
+        throw new Error(err)
+    }
+}
+
 exports.store = async (req, reply) => {
     try {
         const product = new Product(req.body)
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,5 +1,9 @@
 const product = require('../controllers/products.controller')
 const routes = [{
+    method: 'GET',
+    url: '/api/products',
+    handler: product.index
+}, {
     method: 'POST',
     url: '/api/products',
     handler: product.store
